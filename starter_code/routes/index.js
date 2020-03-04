1de74110d8b2968d4b1ca46bc509ae4a72c67663
Tzikas <niko.tzikas@gmail.com>
2020-03-04 17:26:17 -0500

    s

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -3,7 +3,22 @@ const router  = express.Router();
 
 /* GET home page */
 router.get('/', (req, res, next) => {
-  res.render('index');
+  res.render('index.hbs', { name: 'Melvin', name2: 'David'} );
+  //res.json({ name: 'Melvin', name2: 'David'}) this is how its done with react 
 });
+
+
+router.get('/about', (req, res, next) => {
+  res.render('about.hbs', { 
+      greeting: "Welcome to Spanish Class", 
+      verbs: ['correr', 'saltar', 'caminar', 'estudiar', 'bailar', 'matar'],
+      words: [{  adjective: 'lindo'}, {  adjective: 'fao'}, {  adjective: 'bonito'}, {  adjective: 'chismosa'}]
+    })
+})
+
+
+router.get('/newpage', (req, res, next) => {
+  res.render('newpage')
+})
 
 module.exports = router;
